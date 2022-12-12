2d64a1eca5cc34a547854acae733c57aeea61bfb
Esteban Flores <esflores@microsoft.com>
2022-12-12 14:43:26 -0600

    Remove restart lines (#5)
    * remove RESTART lines from sar files
    
    * remove unused utility file

--- src/Utils/callParse.js
+++ src/Utils/callParse.js
@@ -4,7 +4,6 @@ import readData from "./readData";
 
 export function callParse(fileContent) {
 
-
     const sarFileData = readData(fileContent); // Note these functions don't need to be async as data has already been read
     const cpuObject = parseCPUData(sarFileData);
     const blockObject = parseDiskIO(sarFileData);
--- src/Utils/readData.js
+++ src/Utils/readData.js
@@ -1,12 +1,16 @@
 
 
 
-const parseRow = row => { // parses rows and identifies if there's AM/PM or not in the second column, diferentiating between 12hr and 24hr formats (different versions of sar)
+function parseRow (row) { // parses rows and identifies if there's AM/PM or not in the second column, diferentiating between 12hr and 24hr formats (different versions of sar). Also removes RESTART lines
+    if (row.includes("RESTART")) {
+        return ""
+    }
 
     const emptySplit = row.toString().split(' '); // split by empty space
+   
     const commaSplit = emptySplit.toString().split(','); // toString adds a comma at the end, this removes it
     let filtered = commaSplit.filter(entry => {
-    return entry.trim() !== '';  // remove empty spaces from the array
+        return entry.trim() !== '';  // remove empty spaces from the array
     });
 
     if (filtered[1] === 'AM' || filtered[1] === 'PM'  ) {
@@ -19,9 +23,8 @@ const parseRow = row => { // parses rows and identifies if there's AM/PM or not 
 
 }
 
-function readData(data) { //used when uploading file through the dropzone
+export default function readData(data) { //used when uploading file through the dropzone
     try {
-        // console.log(data);
         const rows = data.split('\n'); // split by line break
         const sarData = rows.map(parseRow);
         const sarDataNoSpaces = sarData.filter(element => String(element).trim()); // removes spaces from the sar file (simplifies parsing)
@@ -31,4 +34,3 @@ function readData(data) { //used when uploading file through the dropzone
     }    
 }
 
-export default readData;
