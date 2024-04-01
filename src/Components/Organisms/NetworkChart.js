c7fc420600cab84462de741b8cd3fda017b311b9
Esteban Flores <esflores@microsoft.com>
2024-03-31 19:45:24 -0600

    Add zoom and average calculation functionality to NetworkChart component

--- src/Components/Organisms/NetworkChart.js
+++ src/Components/Organisms/NetworkChart.js
@@ -1,8 +1,10 @@
-import { useMemo, useRef, useEffect } from "react";
-
+import { useMemo, useRef, useEffect, useState } from "react";
 import { useDataContext } from "../Contexts/DataContext";
-import ItemList from "../Atoms/List";
 
+// antd imports
+import { Flex, Typography } from "antd";
+
+// chart.js imports
 import "chartjs-adapter-date-fns";
 import zoomPlugin from "chartjs-plugin-zoom"; // import zoom plugin
 import {
@@ -20,8 +22,12 @@ import {
   Decimation,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+
+// Custom components
 import ResetButton from "../Atoms/ResetButton";
-import { Flex } from "antd";
+import ItemList from "../Atoms/List";
+import CopyClipboardButton from "../Atoms/CopyClipButton";
+
 
 ChartJS.register(
   CategoryScale,
@@ -41,6 +47,42 @@ ChartJS.register(
 export default function NetworkChart() {
   const { netData, selectedInterface, setSelectedInterface } = useDataContext();
   const chartRef = useRef();
+
+  const [zoomLevel, setZoomLevel] = useState(1);
+
+  // Metric states for selected period average calculation
+  const [rxpckAvg, setRxpckAvg] = useState(0);
+  const [txpckAvg, setTxpckAvg] = useState(0);
+  const [rxkBpsAvg, setRxkBpsAvg] = useState(0);
+  const [txkBpsAvg, setTxkBpsAvg] = useState(0);
+
+
+  function fetchData(min, max, chart) {
+    const dataAvgs = [];
+    chart.data.datasets.forEach((dataset) => {
+      // Iterates over the datasets of the current chart
+      const currentData = [];
+      dataset.data.forEach((data) => {
+        // Extracts the data based on the min/max x time values
+        if (data.x >= min && data.x <= max) {
+          currentData.push(data.y);
+        }
+      });
+      const avg =
+        Math.round(
+          (currentData.reduce((acc, curr) => acc + curr, 0) /
+            currentData.length) *
+            100
+        ) / 100; // Calculates the average of the data and round to two decimal places
+
+      dataAvgs.push(avg);
+    });
+
+    setRxpckAvg(dataAvgs[0]);
+    setTxpckAvg(dataAvgs[1]);
+    setRxkBpsAvg(dataAvgs[2]);
+    setTxkBpsAvg(dataAvgs[3]);
+  }
 
   function createChartData() {
     return {
@@ -138,10 +180,23 @@ export default function NetworkChart() {
             },
             mode: "x",
             speed: 0.05,
+            onZoomComplete: function ({ chart }) {
+              setZoomLevel(chart.getZoomLevel()); // Updates zoom level when zoom completes
+              const xAxis = chart.scales.x;
+              const xMin = xAxis.min;
+              const xMax = xAxis.max;
+              fetchData(xMin, xMax, chart);
+            },
           },
           pan: {
             enabled: true,
             mode: "x",
+            onPanComplete: function ({ chart }) {
+              const xAxis = chart.scales.x;
+              const xMin = xAxis.min;
+              const xMax = xAxis.max;
+              fetchData(xMin, xMax, chart);
+            },
           },
         },
         decimation: {
@@ -158,6 +213,12 @@ export default function NetworkChart() {
   const chartOptions = useMemo(() => createChartOptions(), [netData]);
 
   useEffect(() => {
+    if (chartRef.current.scales) {
+      // Update cpu data when selected CPU changes
+      const xMin = chartRef.current.scales.x.min;
+      const xMax = chartRef.current.scales.x.max;
+      fetchData(xMin, xMax, chartRef.current);
+    }
     chartRef.current.update();
   }, [selectedInterface]);
 
@@ -179,6 +240,19 @@ export default function NetworkChart() {
             showSearch={true}
           />
           <ResetButton chartRef={chartRef} />
+          <CopyClipboardButton chartRef={chartRef}/>
+          <Typography.Text type="primary">
+            Current level zoom: {zoomLevel}
+          </Typography.Text>
+          <Typography.Text type="primary">
+            Averages for selected period:
+          </Typography.Text>
+          <Typography.Text type="primary">
+            Receive pckts/s: <b className="text-sky-600">{rxpckAvg}/s</b>, 
+            Transmit pckts/s: <b className="text-amber-500">{txpckAvg}/s</b>, 
+            Receive MB/s: <b className="text-green-500">{rxkBpsAvg}MB/s</b>,
+            Transmit MB/s: <b className="text-red-600">{txkBpsAvg}MB/s</b>,
+          </Typography.Text>
         </Flex>
       ) : (
         <></>
--- src/Components/Organisms/NetworkErrChart.js
+++ src/Components/Organisms/NetworkErrChart.js
@@ -1,8 +1,10 @@
-import { useMemo, useRef, useEffect } from "react";
-
+import { useMemo, useRef, useEffect, useState } from "react";
 import { useDataContext } from "../Contexts/DataContext";
-import ItemList from "../Atoms/List";
 
+// antd imports
+import { Flex, Typography } from "antd";
+
+// chart.js imports
 import "chartjs-adapter-date-fns";
 import zoomPlugin from "chartjs-plugin-zoom"; // import zoom plugin
 import {
@@ -20,8 +22,12 @@ import {
   Decimation,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+
+// Custom components
 import ResetButton from "../Atoms/ResetButton";
-import { Flex } from "antd";
+import CopyClipboardButton from "../Atoms/CopyClipButton";
+import ItemList from "../Atoms/List";
+
 
 ChartJS.register(
   CategoryScale,
@@ -41,6 +47,44 @@ ChartJS.register(
 export default function NetworkErrChart() {
   const { netErrData, selectedInterface, setSelectedInterface } = useDataContext();
   const chartRef = useRef();
+
+  const [zoomLevel, setZoomLevel] = useState(1);
+
+  // Metric states for selected period average calculation
+  const [rxerrAvg, setRxerrAvg] = useState(0);
+  const [txerrAvg, setTxerrAvg] = useState(0);
+  const [collAvg, setCollAvg] = useState(0);
+  const [rxdropAvg, setRxdropAvg] = useState(0);
+  const [txdropAvg, setTxdropAvg] = useState(0);
+
+  function fetchData(min, max, chart) {
+    const dataAvgs = [];
+    chart.data.datasets.forEach((dataset) => {
+      // Iterates over the datasets of the current chart
+      const currentData = [];
+      dataset.data.forEach((data) => {
+        // Extracts the data based on the min/max x time values
+        if (data.x >= min && data.x <= max) {
+          currentData.push(data.y);
+        }
+      });
+      const avg =
+        Math.round(
+          (currentData.reduce((acc, curr) => acc + curr, 0) /
+            currentData.length) *
+            100
+        ) / 100; // Calculates the average of the data and round to two decimal places
+
+      dataAvgs.push(avg);
+    });
+
+    setRxerrAvg(dataAvgs[0]);
+    setTxerrAvg(dataAvgs[1]);
+    setCollAvg(dataAvgs[2]);
+    setRxdropAvg(dataAvgs[3]);
+    setTxdropAvg(dataAvgs[4]);
+  }
+
   function createChartData() {
     return {
       datasets: [
@@ -146,10 +190,23 @@ export default function NetworkErrChart() {
             },
             mode: "x",
             speed: 0.05,
+            onZoomComplete: function ({ chart }) {
+              setZoomLevel(chart.getZoomLevel()); // Updates zoom level when zoom completes
+              const xAxis = chart.scales.x;
+              const xMin = xAxis.min;
+              const xMax = xAxis.max;
+              fetchData(xMin, xMax, chart);
+            },
           },
           pan: {
             enabled: true,
             mode: "x",
+            onPanComplete: function ({ chart }) {
+              const xAxis = chart.scales.x;
+              const xMin = xAxis.min;
+              const xMax = xAxis.max;
+              fetchData(xMin, xMax, chart);
+            },
           },
         },
         decimation: {
@@ -166,6 +223,13 @@ export default function NetworkErrChart() {
   const chartOptions = useMemo(() => createChartOptions(), [netErrData]);
 
   useEffect(() => {
+    if (chartRef.current.scales) {
+      // Update cpu data when selected CPU changes
+      const xMin = chartRef.current.scales.x.min;
+      const xMax = chartRef.current.scales.x.max;
+      fetchData(xMin, xMax, chartRef.current);
+    }
+
     chartRef.current.update();
   }, [selectedInterface]);
 
@@ -187,6 +251,20 @@ export default function NetworkErrChart() {
             showSearch={true}
           />
           <ResetButton chartRef={chartRef} />
+          <CopyClipboardButton chartRef={chartRef}/>
+          <Typography.Text type="primary">
+            Current level zoom: {zoomLevel}
+          </Typography.Text>
+          <Typography.Text type="primary">
+            Averages for selected period:
+          </Typography.Text>
+          <Typography.Text type="primary">
+            Receive pckts/s: <b className="text-sky-600">{rxerrAvg}/s</b>, 
+            Receive MB/s: <b className="text-green-500">{txerrAvg}/s</b>,
+            Transmit MB/s: <b className="text-red-600">{collAvg}/s</b>,
+            Transmit pckts/s: <b className="text-amber-500">{rxdropAvg}/s</b>, 
+            Transmit MB/s: <b className="text-violet-400">{txdropAvg}/s</b>,
+          </Typography.Text>
         </Flex>
       ) : (
         <></>
