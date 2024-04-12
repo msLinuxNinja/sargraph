d8aeb1a32cdacc20674ca4dc81eea7c9dade2b33
Esteban Flores <esflores@microsoft.com>
2024-04-12 11:41:22 -0600

    Update chart colors in BlockIOChart.js

--- src/Utils/colors.js
+++ src/Utils/colors.js
@@ -4,6 +4,7 @@ export const colorConfig = {
     color1: { // Blue
       // CPU usr%
       // Mem Free
+      // I/O tps
       background: "rgba(0, 132, 195, 0.1)",
       border: "rgba(0, 132, 195, 1)",
     },
@@ -15,18 +16,21 @@ export const colorConfig = {
     color2: {// Orange
       //CPU nice%
       // Mem Used
+      // I/O read MB/s
       background: "rgba(254, 140, 0, 0.1)", 
       border: "rgba(254, 140, 0, 1)",
     },
     color3: {// Green
       //CPU sys%
       // Mem Buffers
+      // I/O write MB/s
       background: "rgba(58, 245, 39, 0.1)", 
       border: "rgba(58, 245, 39, 1)",
     },
     color4: {// Red
       // CPU iowait%
       // Mem Cache
+      // I/O avg request size
       background: "rgba(255, 0, 0, 0.1)", 
       border: "rgba(255, 0, 0, 1)",
     },
@@ -39,12 +43,14 @@ export const colorConfig = {
     color6: {// Cherry
       //CPU softirq%
       // Mem Commit
+      // I/O avg queue size
       background: "rgba(177, 17, 82, 0.1)", 
       border: "rgba(177, 17, 82, 1)",
     },
     color7: {// Light Blue
       //CPU idle%
       // Mem Total & Swap Total
+      // I/O latency in MS
       background: "rgba(0, 210, 255, 0.05)", 
       border: "rgba(0, 210, 255, 1)",
     },
