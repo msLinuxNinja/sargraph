adb42dec9753ff1789fc1dfd0ef2730a88d1097f
Esteban Flores <esflores@microsoft.com>
2026-03-16 17:39:37 -0600

    Refactor footer className to remove unnecessary items-center

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -207,7 +207,7 @@ export const HomePage = () => {
         </Drawer>
         <Footer
           style={footerStyle}
-          className="z-30 flex justify-start items-center content-center rounded-b-lg"
+          className="z-30 flex justify-start content-center rounded-b-lg"
         >
           <FooterDetails />
         </Footer>
